681c77ee65f8a3e4fc7110f7a392f519078a3cde
escoffon <>
1998-07-06 14:38:20 +0000

    replaced the SCCS id string with %% keywords

--- generic/tclProc.c
+++ generic/tclProc.c
@@ -10,7 +10,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * SCCS: @(#) tclProc.c 1.116 97/10/29 18:33:24
+ * SCCS: %Z% $Id$ 
  */
 
 #include "tclInt.h"
