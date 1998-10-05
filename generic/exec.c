6284069877b523925ceccc618cdda75e92320fb5
stanton <>
1998-10-05 17:38:26 +0000

    replaced SCCS strings, added initializer to avoid purify warnings

--- generic/exec.c
+++ generic/exec.c
@@ -30,7 +30,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * SCCS: @(#) exec.c 1.10 98/01/21 14:32:57
+ * RCS: @(#) $Id$
  */
 
 #include "tclInt.h"
@@ -1734,6 +1734,7 @@ struct dfa *d;
 			ss->inchain[i].ss = NULL;
 		}
 		ss->flags = 0;
+		ss->ins.co = 0;
 		return ss;
 	}
 
