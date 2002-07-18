1af6ee5d9b57cf8384dc9ab047441fea786e7f91
vincentdarley <>
2002-07-18 16:26:05 +0000

    Tcl_LoadHandle usage

--- generic/tclInt.h
+++ generic/tclInt.h
@@ -1811,7 +1811,7 @@ EXTERN char *		TclpReadlink _ANSI_ARGS_((CONST char *fileName,
 			    Tcl_DString *linkPtr));
 EXTERN void		TclpReleaseFile _ANSI_ARGS_((TclFile file));
 EXTERN void		TclpSetVariables _ANSI_ARGS_((Tcl_Interp *interp));
-EXTERN void		TclpUnloadFile _ANSI_ARGS_((TclLoadHandle loadHandle));
+EXTERN void		TclpUnloadFile _ANSI_ARGS_((Tcl_LoadHandle loadHandle));
 EXTERN VOID *		TclpThreadDataKeyGet _ANSI_ARGS_((
 			    Tcl_ThreadDataKey *keyPtr));
 EXTERN void		TclpThreadDataKeyInit _ANSI_ARGS_((
@@ -1832,10 +1832,10 @@ EXTERN Tcl_Obj*         TclpNativeToNormalized
 EXTERN Tcl_Obj*	        TclpFilesystemPathType
 					_ANSI_ARGS_((Tcl_Obj* pathObjPtr));
 EXTERN Tcl_PackageInitProc* TclpFindSymbol _ANSI_ARGS_((Tcl_Interp *interp,
-			    TclLoadHandle loadHandle, CONST char *symbol));
+			    Tcl_LoadHandle loadHandle, CONST char *symbol));
 EXTERN int              TclpDlopen _ANSI_ARGS_((Tcl_Interp *interp, 
 			    Tcl_Obj *pathPtr, 
-	                    TclLoadHandle *loadHandle, 
+	                    Tcl_LoadHandle *loadHandle, 
 		            Tcl_FSUnloadFileProc **unloadProcPtr));
 
 /*
