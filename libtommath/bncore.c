39613edfbce1c10d07212b7572707e578e4e3a76
kennykb <>
2006-12-01 00:31:32 +0000

    Upgrade to tommath 0.39 plus patches from Tom

--- libtommath/bncore.c
+++ libtommath/bncore.c
@@ -24,7 +24,8 @@
  
 */
 
-int     KARATSUBA_MUL_CUTOFF = 80,      /* Min. number of digits before Karatsuba multiplication is used. */
+int     BZ_DIV_CUTOFF        = 30,      /* Min. number of digits before Burnikel-Ziegler division is used. */
+        KARATSUBA_MUL_CUTOFF = 80,      /* Min. number of digits before Karatsuba multiplication is used. */
         KARATSUBA_SQR_CUTOFF = 120,     /* Min. number of digits before Karatsuba squaring is used. */
         
         TOOM_MUL_CUTOFF      = 350,      /* no optimal values of these are known yet so set em high */
